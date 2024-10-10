93dfea4fb700d4f54ca4966f7bcc5a9c3e47079b
sudorm-rfslash <coupe-00-daylong@icloud.com>
2024-10-09 23:16:28 -0600

    js works

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,9 +1,23 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('______'); // you need to fill in the blank to reference
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
+const list = document.querySelector('ul'); // you need to fill in the blank to reference the HTML element that is a unordered list element.
+
+button.addEventListener('click', function () {
+  if (input.value.trim() !== '') {
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+    li.textContent = input.value;
+    deleteButton.textContent = '❌';
+    li.append(deleteButton);
+    list.append(li);
+    input.focus();
+    input.value = '';
+    
+    deleteButton.addEventListener('click', function () {
+      list.removeChild(li);
+      input.focus();
+      input.value = '';
+    });
+  }
+});
+
