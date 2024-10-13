b1402cf7fc5f3e62d453fe7efef6277e70f2b427
sudorm-rfslash <coupe-00-daylong@icloud.com>
2024-10-12 21:06:41 -0600

    fixed css validation errors

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,9 +1,9 @@
-document.getElementById('toggleButton').addEventListener('click', function() {
+document.getElementById('toggleButton').addEventListener('click', function () {
   const navbar = document.getElementById('navbar');
   navbar.classList.toggle('hidden');
   if (this.textContent === '☰') {
-      this.textContent = 'X';
+    this.textContent = 'X';
   } else {
-      this.textContent = '☰';
+    this.textContent = '☰';
   }
 });
