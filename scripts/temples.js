738c5443773b89725c457dda24357ff3f11de7ff
sudorm-rfslash <coupe-00-daylong@icloud.com>
2024-10-12 20:57:13 -0600

    Finish Temple Picture Album.

---
+++ scripts/temples.js
@@ -0,0 +1,9 @@
+document.getElementById('toggleButton').addEventListener('click', function() {
+  const navbar = document.getElementById('navbar');
+  navbar.classList.toggle('hidden');
+  if (this.textContent === '☰') {
+      this.textContent = 'X';
+  } else {
+      this.textContent = '☰';
+  }
+});
